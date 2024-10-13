0219de9b9f68d4f73e3e4d12afcde36d7b46d86b
carim <carimvel2004@gmail.com>
2024-10-13 17:00:16 -0600

    Elaboración de interfaces gráficas de usuario "Iniciar sesión" y "Crear cuenta", creación del diccionario de internacionalización y aplicado a las GUIs creadas

---
+++ LaOcaClient/CrearCuenta.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LaOcaClient
+{
+    public partial class CrearCuenta : Window
+    {
+        public CrearCuenta()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSeleccionarImagenPerfil_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCrearCuenta_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("¿Estás seguro de que deseas cancelar la creación de la cuenta?", "Confirmación", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                IniciarSesion iniciarSesionWindow = new IniciarSesion();
+                iniciarSesionWindow.Show();
+                this.Close();
+            }
+        }
+    }
+}
---
+++ LaOcaClient/IniciarSesion.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LaOcaClient
+{
+    public partial class IniciarSesion : Window
+    {
+        public IniciarSesion()
+        {
+            InitializeComponent();
+        }
+        private void ManejarFocoTextBox(object remitente, RoutedEventArgs e)
+        {
+            TextBox cuadroTexto = remitente as TextBox;
+            if (cuadroTexto.Text == cuadroTexto.Tag.ToString())
+            {
+                cuadroTexto.Text = "";
+                cuadroTexto.Foreground = Brushes.Black;
+            }
+        }
+
+        private void ManejarPerdidaFocoTextBox(object remitente, RoutedEventArgs e)
+        {
+            TextBox cuadroTexto = remitente as TextBox;
+            if (string.IsNullOrWhiteSpace(cuadroTexto.Text))
+            {
+                cuadroTexto.Text = cuadroTexto.Tag.ToString();
+                cuadroTexto.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void btnIniciarSesion_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnOlvideMiContraseña_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCrearCuentaNueva_Click(object sender, RoutedEventArgs e)
+        {
+            CrearCuenta crearCuentaWindow = new CrearCuenta();
+            crearCuentaWindow.Show();
+            this.Close();
+        }
+
+        private void btnJugarComoInvitado_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
