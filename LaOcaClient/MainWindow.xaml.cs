85b5b57af3ba82b6c2b08db20a064f2399ee357f
carim <carimvel2004@gmail.com>
2024-10-14 07:57:52 -0600

    Implementación de funcionalidad para agregar un jugador a la base de datos, actualización de la referencia de servicio para la interfaz y clase creada para lógica de creación de una cuenta nueva.

--- LaOcaClient/MainWindow.xaml.cs
+++ LaOcaClient/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -32,6 +33,30 @@ namespace LaOcaClient
             int resultado = cliente.GetUser("pepito");
 
             MessageBox.Show("Resultado de la prueba (debería ser 0): " + resultado);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ChannelFactory<IServicioCrearCuenta> channelFactory = new ChannelFactory<IServicioCrearCuenta>(new NetTcpBinding(), new EndpointAddress("net.tcp://localhost:8083/LaOcaServicio"));
+            IServicioCrearCuenta client = channelFactory.CreateChannel();
+            Jugadores nuevoJugador = new Jugadores
+            {
+                nombreUsuario = "carimvch33",
+                nombre = "Carim",
+                apellidoPaterno = "Velázquez",
+                apellidoMaterno = "Chicuellar",
+            };
+            try
+            {
+                client.AgregarJugador(nuevoJugador);
+                Console.WriteLine("Jugador agregado exitosamente.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al agregar el jugador: {ex.Message}");
+            }
+            ((IClientChannel)client).Close();
+            channelFactory.Close();
         }
     }
 }
